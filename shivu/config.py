5840a904868f5cf764a307264f1fb94a5385a2a6
tusarpatel <167279662+tusarpatel@users.noreply.github.com>
2024-05-13 16:29:39 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    OWNER_ID = "6848223695"
+    sudo_users = "6968763387"
+    GROUP_ID = -1002023182491
+    TOKEN = "6850137736:AAGBOBgnxK6SV2LhDNE0HvweCmgOS1Bc9o4"
+    mongo_url = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    PHOTO_URL = ["https://graph.org/file/a436ca4a5af069d400f9d.jpg", "https://graph.org/file/47039f027d6b95345b29d.jpg"]
+    SUPPORT_CHAT = "waifu_catcher_support_pills"
+    UPDATE_CHAT = "waifu_catcher_support_pills"
+    BOT_USERNAME = "waifu_catcher_pills_bot"
+    CHARA_CHANNEL_ID = "-1002023182491"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
