f91cbb852447768522d24df61ef7948ba5258cc5
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-07-26 03:33:21 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,15 +3,15 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6848223695"
-    sudo_users = "6968763387"
-    GROUP_ID = -1002037603190
-    TOKEN = "6850137736:AAGBOBgnxK6SV2LhDNE0HvweCmgOS1Bc9o4"
+    sudo_users = "6848223695"
+    GROUP_ID = -1002023182491
+    TOKEN = "7483313486:AAFcCLC28lL5I16Y3XrAAkcb9UVUpQ3hsqI"
     mongo_url = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://graph.org/file/a436ca4a5af069d400f9d.jpg", "https://graph.org/file/47039f027d6b95345b29d.jpg"]
-    SUPPORT_CHAT = "Nothing_waifu_bot"
-    UPDATE_CHAT = "Nothing_waifu_bot"
+    SUPPORT_CHAT = "Nothing_waifu_support"
+    UPDATE_CHAT = "Nothing_waifu_support"
     BOT_USERNAME = "Nothing_waifu_bot"
-    CHARA_CHANNEL_ID = "-1002037603190"
+    CHARA_CHANNEL_ID = "-1002236086617"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
