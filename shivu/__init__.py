da709c98b248d426da39ff16180097af6aaf2a22
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-10-16 05:20:08 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -19,13 +19,13 @@ LOGGER = logging.getLogger(__name__)
 OWNER_ID =["6965147961","6848223695"]
 SUDO_USERS = ["6848223695","6965147961","5458968679","6864672519"]
 GROUP_ID = -1002332668271
-TOKEN = "7749407611:AAEBsTIO6hRVNiD_tApKeOJVBYJAffGnMtc" 
-mongo_url = "mongodb+srv://snatcherwaifu0925:snatcherwaifu0925@snatcher0.fft3j.mongodb.net/?retryWrites=true&w=majority&appName=Snatcher0"
+TOKEN = "7633847547:AAHSOz3bzzx8iyX61kg8-RSnMHPNxGTPgpM" 
+mongo_url = "postgres://koyeb-adm:yZubkimC56DR@ep-rapid-poetry-a2nmt031.eu-central-1.pg.koyeb.app/koyebdb"
 PHOTO_URL = ["https://envs.sh/nJo.jpg", "https://envs.sh/nJs.jpg"]
 SUPPORT_CHAT = "-1002332668271"
 UPDATE_CHAT = "-1002332668271"
 BOT_USERNAME = "@SNATCH_YOURR_WAIFU_BOT"
-CHARA_CHANNEL_ID = "-1002336624454"
+CHARA_CHANNEL_ID = "-1002251479334"
 api_id = "24835491"
 api_hash = "04ee66f0079a9b11eefb33a89289899e" 
 
